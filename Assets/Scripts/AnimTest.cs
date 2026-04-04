342c38d11107430a0eabfe45aee6bdcccdd8bd47
pskrub <paulskrubbeltrang@gmail.com>
2026-04-04 17:26:14 +0200

    added IK tracking

---
+++ Assets/Scripts/AnimTest.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimTest : MonoBehaviour
+{
+    [SerializeField] private GameObject neckBone;
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 1.0f;
+    private float offset = 156.472f;
+
+    void Update()
+    {
+        Vector3 targetDirection = target.position - neckBone.transform.position;
+        float singleStep = speed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(neckBone.transform.forward, targetDirection, singleStep, 0.0f);
+
+        Debug.DrawRay(neckBone.transform.position, newDirection, Color.red);
+
+        neckBone.transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+}
